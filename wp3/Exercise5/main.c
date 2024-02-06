8e39a22a20e2f2125faba833b2a7abf461876c6b
AliMousa27 <jafar.myhero@gmail.com>
2024-02-06 22:36:53 +0100

    finished 5

---
+++ wp3/Exercise5/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MAX 10
+#define MAX_NUM 100
+
+void random_array(int* array_ptr);
+
+int main(int argc, char* argv[]) {
+    int* array = malloc(sizeof(int) * MAX);
+    random_array(array);
+    printf("The value of the address of the array (pointer) is: %p\n", array);
+    printf("First integer in the array is (array[0]): %d\n", *array);
+    printf("The last integer in the array is: %d\n", array[MAX - 1]);
+    printf("The size of an integer (number of bytes) is: %d\n", sizeof(*array));
+    printf("The size of the whole array in bytes is: %d\n", sizeof(*array) * MAX);
+    for (int i = 0; i < MAX; i++) {
+        printf("Integer: %d,  2 * Integer: %d\n", array[i], array[i] * 2);
+    }
+    free(array);
+}
+
+void random_array(int* array_ptr) {
+    srand(time(NULL));
+    for (int i = 0; i < MAX; i++) {
+        int nr = rand() % MAX_NUM;
+        *array_ptr = nr;
+        array_ptr++;
+    }
+}
